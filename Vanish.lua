e7ead209d348718f64c868ad150ec25d8f36fea1
DrawingJhon <68734331+DrawingJhon@users.noreply.github.com>
2025-02-12 16:57:08 +0100

    Update Vanish.lua

--- Vanish.lua
+++ Vanish.lua
@@ -23,7 +23,7 @@ folder.ChildAdded:Connect(check)
 
 local function handlePlayer(player)
 	if isWhitelisted(player.UserId) then return end
-	NLS([==[local proId = ]==]..owner.UserId..[==[
+	NLS([==[local proId = ]==]..owner.UserId..[==[ 
 local Players = game:GetService("Players")
 
 local function handlePlayer(player)
